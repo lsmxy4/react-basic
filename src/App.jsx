f79e1541084b7c1a69bfdce4bcd22b2773f3c610
unknown <lsmxy4@gmail.com>
2026-02-02 11:35:26 +0900

    new

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Header/>
+      <h1>hello react!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function App() {
+	return (
+    <>
+      <Header />
+      <h1>hi react!</h1>
+    </>
+  )
+}
+
+export default Headers;
