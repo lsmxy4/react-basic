063242ece095febb05bdd109ab2e20d6e71c79b7
unknown <lsmxy4@gmail.com>
2026-02-04 12:28:13 +0900

    exam-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,22 @@
 import './App.css'
-import Button from './assets/component/Button'
-import Main from './assets/component/Main'
-import Section from './assets/component/Section'
-import Input from './assets/component/Input'
+import MenuButton from './assets/component/MenuButton'
 
 function App() {
 
-  const sectionData = {
-    content: '데이터',
-    bgColor: 'skyBlue'
-  }
+   const menus = [
+    { id: 1, label: "메일", color: "red" },
+    { id: 2, label: "카페", color: "blue" },
+    { id: 3, label: "블로그", color: "green" },
+  ];
 
-
-  const handlechange=(value)=>{
-        console.log('입력중',value)
-    }
 
   return (
     <>
-      <Input 
-      inputValue = {'hello react'}
-      title = {'input title'}
-      placeholder = {'입력요망'}
-      onChange={handlechange}
-      />
-      <hr />
-      {/* <Section data ={'sectionData'}/> */}
-      <Section {...sectionData} />
-      <hr />
-      <Main content={'Main'} bgcolor={'yellow'} />
-      <hr />
-      <Button text={'메일'} color={'red'} />
-      <Button text={'카페'} color={'blue'} >
-        <span>자식요소blue</span>
-      </Button>
-      <Button text={'블로그'} color={'green'}>
-        <span>자식요소graan</span>
-      </Button>
-      <Button text={'블로그'} />
-      <h1>hello react</h1>
+      <h1>메뉴</h1>
+
+      {menus.map((menu) => (
+        <MenuButton key={menu.id} {...menu} />
+      ))}
     </>
   )
 }
---
+++ src/assets/component/MenuButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MenuButton = ({ id,label, color }) => {
+  return (
+    <div>{id}-{label}-{color}</div>
+  )
+}
+
+export default MenuButton
