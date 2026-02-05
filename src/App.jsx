66cd2bb83d18cd0320b2fe965ff6c0e0a00b720b
unknown <lsmxy4@gmail.com>
2026-02-05 12:06:46 +0900

    exam-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import './App.css'
 import Counter from './assets/component/Counter'
 import Exam1 from './assets/component/Exam1'
@@ -5,27 +6,35 @@ import Exam2 from './assets/component/Exam2'
 import Exam3 from './assets/component/Exam3'
 import InputFocus from './assets/component/InputFocus'
 import Counter2 from './assets/component/Counter2'
+import LifeCycle from './assets/component/LifeCycle'
 
 function App() {
-    const user={
-    name:'Alice',
-    age:21
+  const [state, setState] = useState(true)
+  const user = {
+    name: 'Alice',
+    age: 21
   }
 
-return (
-  <>
-    <div>
-      <h2>useStatus 연습</h2>
-      <InputFocus/>
-      <Counter2/>
-      <hr />
-      <Exam3 />
-      <Exam1 name={user.name} age={user.age}/>
-      <Exam2 />
-      <Counter />
-    </div>
-  </>
-)
+  return (
+    <>
+      <div>
+        <h2>useStatus 연습</h2>
+        <button onClick={()=>setState(true)}>마운트</button>
+        <button onClick={()=>setState(false)}>언마운트</button>
+        {state &&
+          <LifeCycle />
+        }
+        <hr />
+        <InputFocus />
+        <Counter2 />
+        <hr />
+        <Exam3 />
+        <Exam1 name={user.name} age={user.age} />
+        <Exam2 />
+        <Counter />
+      </div>
+    </>
+  )
 }
 
 export default App
